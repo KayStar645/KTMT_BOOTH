84d1ce68e464085dcabc81c1800c632832d67957
PHẠM TẤN THUẬN <84188413+KayStar645@users.noreply.github.com>
2021-10-19 21:40:41 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace DeTai12_KTMT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBit = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNhapSoBit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBiNhan = new System.Windows.Forms.Label();
+            this.btnNhan = new System.Windows.Forms.Label();
+            this.txtNhapSoNhan = new System.Windows.Forms.TextBox();
+            this.txtNhapSoBiNhan = new System.Windows.Forms.TextBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.btnKetQua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.txtSNNP = new System.Windows.Forms.TextBox();
+            this.txtSBNNP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBit
+            // 
+            this.btnBit.AutoSize = true;
+            this.btnBit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBit.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.btnBit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnBit.Location = new System.Drawing.Point(142, 82);
+            this.btnBit.MinimumSize = new System.Drawing.Size(175, 39);
+            this.btnBit.Name = "btnBit";
+            this.btnBit.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnBit.Size = new System.Drawing.Size(175, 39);
+            this.btnBit.TabIndex = 0;
+            this.btnBit.Text = "Nhập số bit";
+            this.btnBit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // txtNhapSoBit
+            // 
+            this.txtNhapSoBit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtNhapSoBit.Location = new System.Drawing.Point(379, 85);
+            this.txtNhapSoBit.Name = "txtNhapSoBit";
+            this.txtNhapSoBit.Size = new System.Drawing.Size(309, 30);
+            this.txtNhapSoBit.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.Location = new System.Drawing.Point(282, 9);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.label3.Size = new System.Drawing.Size(304, 43);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "NHÂN 2 SỐ NGUYÊN CÓ DẤU";
+            // 
+            // btnBiNhan
+            // 
+            this.btnBiNhan.AutoSize = true;
+            this.btnBiNhan.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBiNhan.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.btnBiNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnBiNhan.Location = new System.Drawing.Point(23, 164);
+            this.btnBiNhan.MinimumSize = new System.Drawing.Size(175, 39);
+            this.btnBiNhan.Name = "btnBiNhan";
+            this.btnBiNhan.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnBiNhan.Size = new System.Drawing.Size(175, 39);
+            this.btnBiNhan.TabIndex = 2;
+            this.btnBiNhan.Text = "Nhập số bị nhân";
+            this.btnBiNhan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.AutoSize = true;
+            this.btnNhan.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnNhan.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.btnNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnNhan.Location = new System.Drawing.Point(23, 238);
+            this.btnNhan.MinimumSize = new System.Drawing.Size(175, 39);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnNhan.Size = new System.Drawing.Size(175, 39);
+            this.btnNhan.TabIndex = 3;
+            this.btnNhan.Text = "Nhập số nhân";
+            this.btnNhan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtNhapSoNhan
+            // 
+            this.txtNhapSoNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtNhapSoNhan.Location = new System.Drawing.Point(234, 238);
+            this.txtNhapSoNhan.Name = "txtNhapSoNhan";
+            this.txtNhapSoNhan.Size = new System.Drawing.Size(132, 30);
+            this.txtNhapSoNhan.TabIndex = 4;
+            // 
+            // txtNhapSoBiNhan
+            // 
+            this.txtNhapSoBiNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtNhapSoBiNhan.Location = new System.Drawing.Point(234, 167);
+            this.txtNhapSoBiNhan.Name = "txtNhapSoBiNhan";
+            this.txtNhapSoBiNhan.Size = new System.Drawing.Size(132, 30);
+            this.txtNhapSoBiNhan.TabIndex = 5;
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.txtKetQua.Location = new System.Drawing.Point(169, 410);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.ReadOnly = true;
+            this.txtKetQua.Size = new System.Drawing.Size(577, 38);
+            this.txtKetQua.TabIndex = 7;
+            this.txtKetQua.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnKetQua
+            // 
+            this.btnKetQua.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnKetQua.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnKetQua.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnKetQua.Location = new System.Drawing.Point(169, 310);
+            this.btnKetQua.Name = "btnKetQua";
+            this.btnKetQua.Padding = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnKetQua.Size = new System.Drawing.Size(197, 64);
+            this.btnKetQua.TabIndex = 8;
+            this.btnKetQua.Text = "KẾT QUẢ";
+            this.btnKetQua.UseVisualStyleBackColor = false;
+            this.btnKetQua.Click += new System.EventHandler(this.btnKetQua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnXoa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnXoa.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnXoa.Location = new System.Drawing.Point(549, 310);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Padding = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.btnXoa.Size = new System.Drawing.Size(197, 64);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "XÓA";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnThoat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnThoat.Location = new System.Drawing.Point(730, 479);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(112, 42);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // txtSNNP
+            // 
+            this.txtSNNP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtSNNP.Location = new System.Drawing.Point(661, 238);
+            this.txtSNNP.Name = "txtSNNP";
+            this.txtSNNP.ReadOnly = true;
+            this.txtSNNP.Size = new System.Drawing.Size(132, 30);
+            this.txtSNNP.TabIndex = 5;
+            this.txtSNNP.TextChanged += new System.EventHandler(this.txtSNNP_TextChanged);
+            // 
+            // txtSBNNP
+            // 
+            this.txtSBNNP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtSBNNP.Location = new System.Drawing.Point(661, 164);
+            this.txtSBNNP.Margin = new System.Windows.Forms.Padding(0);
+            this.txtSBNNP.Name = "txtSBNNP";
+            this.txtSBNNP.ReadOnly = true;
+            this.txtSBNNP.Size = new System.Drawing.Size(132, 30);
+            this.txtSBNNP.TabIndex = 5;
+            this.txtSBNNP.TextChanged += new System.EventHandler(this.txtSBNNP_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(411, 164);
+            this.label1.MinimumSize = new System.Drawing.Size(175, 39);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.label1.Size = new System.Drawing.Size(225, 39);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "SBN đổi sang Nhị phân";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(411, 235);
+            this.label4.MaximumSize = new System.Drawing.Size(225, 39);
+            this.label4.MinimumSize = new System.Drawing.Size(225, 39);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.label4.Size = new System.Drawing.Size(225, 39);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "SN đổi sang Nhị phân";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 521);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSBNNP);
+            this.Controls.Add(this.txtSNNP);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnKetQua);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtNhapSoBiNhan);
+            this.Controls.Add(this.txtNhapSoNhan);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnBiNhan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNhapSoBit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBit);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đề tài 12";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label btnBit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNhapSoBit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label btnBiNhan;
+        private System.Windows.Forms.Label btnNhan;
+        private System.Windows.Forms.TextBox txtNhapSoNhan;
+        private System.Windows.Forms.TextBox txtNhapSoBiNhan;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Button btnKetQua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.TextBox txtSNNP;
+        private System.Windows.Forms.TextBox txtSBNNP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeTai12_KTMT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Tạo mảng kí tự rỗng
+        public char[] tam11 = null;
+
+        //Click vào kết quả để hiển thị ra màn hình kết quả
+        private void btnKetQua_Click(object sender, EventArgs e)
+        {
+            //Kiểm tra đã nhập đủ dữ liệu chưa
+            if (txtNhapSoBit.Text != "" && txtNhapSoBiNhan.Text != "" && txtNhapSoNhan.Text != "")
+            {
+                //Lỗi không chứa toàn số
+                if (kiemTraChuoiKiSo(txtNhapSoBit.Text) == false
+                || kiemTraChuoiKiSo(txtNhapSoBiNhan.Text) == false
+                || kiemTraChuoiKiSo(txtNhapSoNhan.Text) == false)
+                {
+                    MessageBox.Show("Vui lòng chỉ nhập vào số!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (txtNhapSoBit.Text[0] == '-')
+                {
+                    MessageBox.Show("Số bit không được âm!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }    
+                else
+                {
+                    //Số bit
+                    int soBit = Convert.ToInt32(txtNhapSoBit.Text);
+
+                    //Số bị nhân
+                    int sbn = Convert.ToInt32(txtNhapSoBiNhan.Text);
+                    
+                    //Số nhân
+                    int sn = Convert.ToInt32(txtNhapSoNhan.Text);
+
+                    //Trường hợp số nhân hoặc bị nhân quá lớn so với sức biểu diễn của số bit
+                    if (-Math.Pow(2, soBit - 1) > sbn || Math.Pow(2, soBit - 1) - 1 < sbn ||
+                        -Math.Pow(2, soBit - 1) > sn || Math.Pow(2, soBit - 1) - 1 < sn)
+                    {
+                        MessageBox.Show("Số bị nhân hoặc số nhân nằm ngoài giới hạn của bit!" +
+                            "\nVui lòng nhập lại!", "Thông báo",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        char[] soBiNhan = new char[soBit];
+                        soBiNhan = chuyenHe10Sang2(sbn, soBiNhan, soBit);
+
+                        char[] soNhan = new char[soBit];
+                        soNhan = chuyenHe10Sang2(sn, soNhan, soBit);
+
+                        txtSBNNP_TextChanged(sender, e);
+                        txtSNNP_TextChanged(sender, e);
+
+                        //Kết quả
+                        char[] tam = tichNhiPhan(soBiNhan, soNhan, soBit);
+                        string kq = new string(tam11);
+                        for (int i = 0; i < tam.Length; i++)
+                            kq += tam[i];
+                        txtKetQua.Text = kq;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đủ dữ liệu!", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //Tính số bù 2 của số nhị phân
+        public static char[] soBu2(char[] s)
+        {
+            char[] tam = new char[s.Length];
+            for (int i = 0; i < s.Length; i++)
+                tam[i] = s[i];
+            for (int i = tam.Length - 1; i >= 0; i--)
+                if (tam[i] == '1')
+                {
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        if (tam[j] == '0')
+                            tam[j] = '1';
+                        else
+                            tam[j] = '0';
+                    }
+                    break;
+                }
+            return tam;
+        }
+
+        //Chuyển số thập phân sang nhị phân có n bit
+        public static char[] chuyenHe10Sang2(int x, char[] s, int n)
+        {
+            int dau = 0;
+            if (x < 0)
+            {
+                dau = 1;
+                x = Math.Abs(x);
+            }
+            int i = n - 1;
+            while (x != 0 && i >= 0)
+            {
+                s[i] = Convert.ToChar(x % 2 + 48);
+                x /= 2;
+                i--;
+            }
+            //Nếu chưa đủ n bit thì thêm vào cho đủ
+            for (int j = 0; j < s.Length; j++)
+            {
+                if (s[j] == '\0')
+                    s[j] = '0';
+                else
+                    break;
+            }
+            if (dau == 1)
+                s = soBu2(s);
+            return s;
+        }
+
+        //Xuất số nhị phân ra màn hình
+        public static void xuatNhiPhan(char[] s)
+        {
+            foreach (char c in s)
+                Console.Write("{0}", c);
+        }
+
+        //Công thức cộng nhị phân
+        public static char phepCong(char a, char b)
+        {
+            if (a == '0' && b == '0' || a == '1' && b == '1')
+                return '0';
+            return '1';
+        }
+
+        //Phép cộng 2 số nhị phân
+        public static char[] congNhiPhan(char[] a, char[] b, int n)
+        {
+            char[] kq = new char[n];
+            //Cộng từ phải sang trái
+            for (int i = kq.Length - 1; i >= 0; i--)
+            {
+                if (kq[i] == '\0')
+                    kq[i] = '0';
+                char tam = kq[i];
+                kq[i] = phepCong(kq[i], phepCong(a[i], b[i]));
+                if (a[i] == '1' && b[i] == '1' || phepCong(a[i], b[i]) == '1' && tam == '1')
+                {
+                    if (i == 0)
+                        break;
+                    kq[i - 1] = phepCong(kq[i - 1], '1');
+                }
+            }
+            return kq;
+        }
+
+        //Chèn kí tự x vào đầu và xóa kí tự cuối
+        public static char[] dichPhaiThemX(char[] s, char x)
+        {
+            for (int i = s.Length - 1; i > 0; i--)
+                s[i] = s[i - 1];
+            s[0] = x;
+            return s;
+        }
+
+        //Hàm tính tích 2 số nhị phân
+        public static char[] tichNhiPhan(char[] M, char[] Q, int n)
+        {
+            char[] tich = new char[n * 2];
+            char[] A = new char[n];
+            for (int i = 0; i < n; i++)
+                A[i] = '0';
+            char Q1 = '0';
+            char[] sbM = soBu2(M);
+             
+            for (int i = 0; i < n; i++)
+            {
+                if (Q[Q.Length - 1] == Q1)
+                {
+                    //Dịch phải  A, Q, Q1
+                    Q1 = Q[Q.Length - 1];
+                    Q = dichPhaiThemX(Q, A[A.Length - 1]);
+                    A = dichPhaiThemX(A, A[0]);
+                }
+                else if (Q[Q.Length - 1] > Q1) //10
+                {
+                    A = congNhiPhan(A, sbM, n);
+                    Q1 = Q[Q.Length - 1];
+                    Q = dichPhaiThemX(Q, A[A.Length - 1]);
+                    A = dichPhaiThemX(A, A[0]);
+                }
+                else  //01
+                {
+                    A = congNhiPhan(A, M, n);
+                    Q1 = Q[Q.Length - 1];
+                    Q = dichPhaiThemX(Q, A[A.Length - 1]);
+                    A = dichPhaiThemX(A, A[0]);
+                }
+            }
+            A.CopyTo(tich, 0);
+            Q.CopyTo(tich, A.Length);
+            return tich;
+        }
+
+        //Kiểm tra trong mảng có toàn kí số hay không
+        public static bool kiemTraChuoiKiSo(string s)
+        {
+            //Nếu kí tự đầu k phải là dấu hoặc số thì sai
+            if (s[0] != '-' && s[0] != '+' && (s[0] < 48 || 57 < s[0]))
+                return false;
+            for (int i = 1; i < s.Length; i++)
+                if (s[i] < 48 || 57 < s[i])
+                    return false;
+            return true;
+        }
+        
+
+        //Xóa các dữ liệu
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            txtKetQua.Clear();
+            txtNhapSoBiNhan.Clear();
+            txtNhapSoBit.Clear();
+            txtNhapSoNhan.Clear();
+            txtSBNNP.Clear();
+            txtSNNP.Clear();
+        }
+
+        //Thoát chương trình
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dlr = MessageBox.Show("Bạn muốn thoát chương trình ??", "Thông Báo",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dlr == DialogResult.Yes)
+            {
+                this.Close();
+                Application.Exit();
+            }
+        }
+
+        //Xuất ra kết quả số bị nhân ở dạng nhị phân
+        private void txtSBNNP_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNhapSoBiNhan.Text != "")
+            {
+                int soBit = Convert.ToInt32(txtNhapSoBit.Text);
+
+                int sbn = Convert.ToInt32(txtNhapSoBiNhan.Text);
+                char[] soBiNhan = new char[soBit];
+                soBiNhan = chuyenHe10Sang2(sbn, soBiNhan, soBit);
+
+                string kq = new string(tam11);
+                for (int i = 0; i < soBiNhan.Length; i++)
+                    kq += soBiNhan[i];
+                txtSBNNP.Text = kq;
+            }    
+        }
+
+        //Xuất ra kết quả số nhân ở dạng nhị phân
+        private void txtSNNP_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNhapSoNhan.Text != "")
+            {
+                int soBit = Convert.ToInt32(txtNhapSoBit.Text);
+
+                int sn = Convert.ToInt32(txtNhapSoNhan.Text);
+                char[] soNhan = new char[soBit];
+                soNhan = chuyenHe10Sang2(sn, soNhan, soBit);
+
+                string kq = new string(tam11);
+                for (int i = 0; i < soNhan.Length; i++)
+                    kq += soNhan[i];
+                txtSNNP.Text = kq;
+            }    
+        }
+    }
+}
